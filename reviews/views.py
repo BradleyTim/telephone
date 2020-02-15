0c734d9ed8b8ac1015a80f71047c7a8cccb07f5c
BradleyTim <timbradley94@gmail.com>
2020-02-15 19:39:36 +0300

    add first render of reviews app

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Review
 
-# Create your views here.
+def index(request):
+    context = {
+        'reviews': Review.objects.all()
+    }
+    return render(request, 'reviews/index.html', context)
