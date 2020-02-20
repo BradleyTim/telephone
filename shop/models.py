b1e9f2c755f62c043e544d59ae7f67e9db2b67d3
BradleyTim <timbradley94@gmail.com>
2020-02-20 22:15:09 +0300

    resize uploaded images to 500 x 500

--- shop/models.py
+++ shop/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from PIL import Image
 
 class Phone(models.Model):
     image = models.ImageField(upload_to='phone_photos/%Y/%m/%d', blank=True)
@@ -14,3 +15,11 @@ class Phone(models.Model):
 
     def __str__(self):
         return self.name
+
+    def save(self):
+        super().save()
+        img = Image.open(self.image.path)
+        if img.height > 500 or img.width > 500:
+            output_size = (500, 500)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
